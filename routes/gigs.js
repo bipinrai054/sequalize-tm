ce9fe30ecb87a3e7d0575ace26da0802b255c1a9
bipnrai054 <bipinrai054@gmail.com>
2022-09-28 16:21:25 +0545

    fetch all and create one done

--- routes/gigs.js
+++ routes/gigs.js
@@ -4,6 +4,7 @@ import Gig from '../models/Gig.js';
 
 const router = express.Router();
 
+// get gigs
 router.get('/', (req, res) =>
   Gig.findAll()
     .then((gigs) => {
@@ -12,5 +13,28 @@ router.get('/', (req, res) =>
     })
     .catch((err) => console.log(err)),
 );
+
+// add a gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'Simple Wordpress Website',
+    technologies: 'worpress,php, html, css',
+    budget: '$1000',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+    contact_email: 'user2@gmail.com',
+  };
+  let { title, technologies, budget, description, contact_email } = data;
+
+  Gig.create({
+    title,
+    technologies,
+    budget,
+    description,
+    contact_email,
+  })
+    .then((gig) => res.redirect('/gigs'))
+    .catch((err) => console.log(err));
+});
 
 export default router;
